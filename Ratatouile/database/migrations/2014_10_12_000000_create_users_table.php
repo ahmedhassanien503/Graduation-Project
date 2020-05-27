325c599ef122ee3d830ac7e5873ac2c75ddc901f
ahmedhassanien503 <ahm_hassanien@yahoo.com>
2020-05-28 00:26:26 +0200

    adding user,questions,answers, workshop and comment also applicant model

--- Ratatouile/database/migrations/2014_10_12_000000_create_users_table.php
+++ Ratatouile/database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,17 +19,11 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-<<<<<<< HEAD
             $table->string('work_place');
             $table->string('image')->nullable();
             $table->boolean('is_chef')->default(false);
             $table->boolean('is_banned')->default(false);
             $table->boolean('is_admin')->default(false);
-=======
-            $table->boolean('is_admin')->default(false);
-            $table->boolean('is_chef')->default(false);
-            $table->string('image')->nullable();
->>>>>>> c08874dafe81accf8da41c5331ce203ffd0c6ec8
             $table->rememberToken();
             $table->timestamps();
         });

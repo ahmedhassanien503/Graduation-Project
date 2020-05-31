17ed913a0d4f6f6deb1ecb222a9e4064b06965ae
manarelsayed95 <manarelsayed969@gmail.com>
2020-05-31 21:45:17 +0200

    edit date of chef migration

--- Ratatouile/app/Order.php
+++ Ratatouile/app/Order.php
@@ -3,40 +3,8 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-use App\MenuRecipes;
-use App\User;
 
 class Order extends Model
 {
-    protected $fillable=[
-        'date',
-        'address',
-        'user_id',
-        'chef_id',
-        'total_price',
-    ];
-
-    /**
-        * The user that request  the order.
-    */
-    public function users()
-    {
-        return $this->belongsTo('App\User');
-    }
-
-    /**
-    * The chef that request  the order.
-    */
-    public function chefs()
-    {
-        return $this->belongsTo('App\Chef');
-    }
-
-    /**
-        * The recipes that belong to the order.
-    */
-    public function menuRecipes()
-    {
-        return $this->belongsToMany('App\MenuRecipes','order_menu_recipes');
-    }
+    //
 }

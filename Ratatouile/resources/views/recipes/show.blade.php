753182875b57bf4484715ec71b3c860c5875fa5b
ahmedhassanien503 <ahm_hassanien@yahoo.com>
2020-06-01 21:42:24 +0200

    update show page

--- Ratatouile/resources/views/recipes/show.blade.php
+++ Ratatouile/resources/views/recipes/show.blade.php
@@ -1,49 +1,26 @@
 @extends('layouts.admin')
 @section('content')
-<div class="d-flex align-content-stretch flex-wrap" style="text-align:center">
-    <div class="container " style="text-align:center">
+<div class="card mb-3">
+  <img src="{{asset('uploads/recipes/'.$recipe->image)}}" class="card-img-top" alt="..." style="width:1300px;height:300px;>
 
-      
-      <section class="content-header">
-          <div class="container-fluid">
-            <div class="row mb-2">
-              <div class="col-sm-6">
-                <h1>Recipe</h1>
-              </div>
-              <div class="col-sm-6">
-                <ol class="breadcrumb float-sm-right">
-                  <li class="breadcrumb-item"><a href="#">Home</a></li>
-                  <li class="breadcrumb-item active">Recipe</li>
-                </ol>
-              </div>
-            </div>
-          </div>
-        </section>
+  <div class="card-body">
+    <h1 class="card-title" style="text-align: center">{{$recipe->RecipeName}}</h1>
+    <p class="card-text">{{$recipe->details}}</p>
+    <p class="card-text"><small class="text-muted"> {{$recipe->created_at}}</small></p>
+    <p class="card-text"><small class="text-muted"> {{$recipe->updated_at}}</small></p>
 
-        {{-- <div class="card card-primary card-outline">
-          <div class="card-body box-profile">
-            <div class="text-center">
-              <img class="profile-user-img img-fluid img-circle"
-                   src="{{asset('uploads/pharmacies/'.$pharmacy->image)}}"
-                   alt="User profile picture">
-            </div> --}}
-      
-  
-        <section class="content">
-      
-          <div class="card">
-            <div class="card-header">
-              <h2 class="card-text">recipe {{ $recipe->id }} Information</h2>
-              <div class="card-body">
-              <br>
-              <strong><i class="fas fa-book mr-1"></i>Recipe Name</strong>
-           {{ $recipe->RecipeName }}
 
-          
-          <h5 class="card-text"> <strong><i class="fas fa-pencil-alt mr-1"></i> Serving </strong>{{ $recipe->Serving  }}</h5>
-          {{-- <h5 class="card-text">Nationa Id:- {{ $pharmacy->national_id}}</h5>
-          <h5 class="card-text">area:- {{ $pharmacy->area_id }}</h5> --}}
-        </div>
-      </div>
+
+
+{{$recipe->Serving}}
+{{-- <p class="text-muted">{{$workshop->created_at}}</p> --}}
+{{$recipe->TakenTime}}
+{{$recipe->user_id}}
+{{$recipe->chef_id}}
+
+  </div>
+</div>
+
+
       
       @endsection

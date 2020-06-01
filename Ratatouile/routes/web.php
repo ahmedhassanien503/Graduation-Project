0ded9a93cd460ce48737b0eb1128b40480d7cace
MayarAlaa1 <mayar.alaa188@gmail.com>
2020-06-02 00:35:11 +0200

    after merge

--- Ratatouile/routes/web.php
+++ Ratatouile/routes/web.php
@@ -12,12 +12,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
 Route::get('/', function () {
-    return  redirect()->route('login');
-    // return view('welcome');
-});
-
-
+    return view('welcome');
+    }); 
 
 Route::group(['middleware' => ['auth:sanctum']], function () {
     Route::get('/admin', function () {return view('admin');})->name('admin.index');

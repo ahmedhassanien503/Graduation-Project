529407198cf96059d070d299e8b6d67fc3914b90
manarelsayed95 <manarelsayed969@gmail.com>
2020-06-04 00:53:31 +0200

    nav-bar & footer

--- src/views/LandingPage/LandingPage.js
+++ src/views/LandingPage/LandingPage.js
@@ -37,7 +37,7 @@ export default function LandingPage(props) {
       <Header
         color="transparent"
         routes={dashboardRoutes}
-        brand="Material Kit React"
+        brand="Ratatouile"
         rightLinks={<HeaderLinks />}
         fixed
         changeColorOnScroll={{
